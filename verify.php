3d2c70f2436958bc47b77d5a6ad4fca441df6889
Classes39 <153883076+Classes39@users.noreply.github.com>
2023-12-27 15:47:07 -0600

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,20 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registration Form</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Registration Form</h2>
-        <form action="send_code.php" method="post">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Send Code</button>
-        </form>
-    </div>
-</body>
-</html>
-
+<?php
+    // Code converted from HTML to PHP
+    echo '<!DOCTYPE html>';
+    echo '<html>';
+    echo '<head>';
+    echo '<title>Registration Form</title>';
+    echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">';
+    echo '</head>';
+    echo '<body>';
+    echo '<div class="container">';
+    echo '<h2>Registration Form</h2>';
+    echo '<form action="send_code.php" method="post">';
+    echo '<div class="form-group">';
+    echo '<label for="email">Email:</label>';
+    echo '<input type="email" class="form-control" id="email" name="email" required>';
+    echo '</div>';
+    echo '<button type="submit" class="btn btn-primary">Send Code</button>';
+    echo '</form>';
+    echo '</div>';
+    echo '</body>';
+    echo '</html>';
+?>
